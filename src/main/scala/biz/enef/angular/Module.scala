d797ca58d7c919106eb6ec7358837a8adbd0aafa
jokade <jkspam@karchedon.de>
2015-01-08 21:48:13 +0100

    Complete Angular facade for Module
    Closes #4

--- src/main/scala/biz/enef/angular/Module.scala
+++ src/main/scala/biz/enef/angular/Module.scala
@@ -1,5 +1,5 @@
 // -   Project: scalajs-angulate (https://github.com/jokade/scalajs-angulate)
-// Description:
+// Description: Facade and enhancements for AngularJS Module API
 //
 // Copyright (c) 2015 Johannes Kastner <jkspam@karchedon.de>
 //               Distributed under the MIT License (see included file LICENSE)
@@ -9,11 +9,71 @@ import scala.language.experimental.macros
 import scala.scalajs.js
 
 /**
- * Defines the bindings to the angular.Module API.
+ * Defines the bindings to the angular.Module API and enhancements provided by scalajs-angulate.
  *
  * @see [[https://docs.angularjs.org/api/ng/type/angular.Module]]
  */
 trait Module  extends js.Object {
+
+  //------------------------------ ANGULAR.JS --------------------------------
+  /**
+   * The name of the module
+   */
+  def name: String
+
+  /**
+   * Defines an animation hook that can be later used with the $animate service and directives that use this service.
+   *
+   * @note animations take effect only if the ngAnimate module is loaded
+   *
+   * @param name animation name
+   * @param animationFactory Factory function
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#animate]]
+   */
+  def animation(name: String, animationFactory: js.Function) : Module
+
+  /**
+   * Defines an animation hook that can be later used with the $animate service and directives that use this service.
+   *
+   * @note animations take effect only if the ngAnimate module is loaded
+   *
+   * @param name animation name
+   * @param animationFactory Array with the names of the dependencies to be injected.
+   *                         The last element in this array must be the factory function
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#animate]]
+   */
+  def animation(name: String, animationFactory: js.Array[Any]) : Module
+
+  /**
+   * Use this method to register work which needs to be performed on module loading.
+   *
+   * @param configFn This function is executed on module load
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#config]]
+   */
+  def config(configFn: js.Function) : Module
+
+  /**
+   * Use this method to register work which needs to be performed on module loading.
+   *
+   * @param configFn Array with the names of the dependencies to be injected.
+   *                 The last element in this array must be the function to be called on module load
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#config]]
+   */
+  def config(configFn: js.Array[Any]) : Module
+
+  /**
+   * Register a constant service, such as a string, number, array ...
+   *
+   * @param name The name of the constant
+   * @param value The constant value
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#constant]]
+   */
+  def constant(name: String, value: js.Any) : Module
 
   /**
    * Registers a controller.
@@ -25,19 +85,142 @@ trait Module  extends js.Object {
    */
   def controller(name: String, constructor: js.Function) : Module
 
+  /**
+   * Registers a controller.
+   *
+   * @param name The name of the controller
+   * @param constructor Array containing the names of the dependencies to be injected and
+   *                    the constructor function as last element
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#controller]]
+   */
   def controller(name: String, constructor: js.Array[Any]) : Module
+
+  /**
+   * Register a new directive with the compiler.
+   *
+   * @param name Name of the directive in camel-case (ie `ngBind`)
+   * @param directiveFactory Directive constructor function
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#directive]]
+   */
+  def directive(name: String, directiveFactory: js.Function) : Module
+
+  /**
+   * Register a new directive with the compiler.
+   *
+   * @param name Name of the directive in camel-case (ie `ngBind`)
+   * @param directiveFactory Array containing the names of the dependencies to be injected and
+   *                         the constructor function as last element
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#directive]]
+   */
+  def directive(name: String, directiveFactory: js.Array[Any]) : Module
 
   /**
    * Register a service factory.
    *
-   * @param name
-   * @param constructor
-   * @return
+   * @param name The name of the service
+   * @param constructor Service constructor function
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#factory]]
    */
   def factory(name: String, constructor: js.Function) : Module
 
+  /**
+   * Register a service factory.
+   *
+   * @param name The name of the service
+   * @param constructor Array containing the names of the dependencies to be injected and
+   *                    the constructor function as last element
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#factory]]
+   */
   def factory(name: String, constructor: js.Array[Any]) : Module
 
+  /**
+   * Register a filter factory.
+   *
+   * @param name The name of the filter
+   * @param filterFactory Filter constructor function
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#filter]]
+   */
+  def filter(name: String, filterFactory: js.Function) : Module
+
+  /**
+   * Register a filter factory.
+   *
+   * @param name The name of the filter
+   * @param filterFactory Array containing the names of the dependencies to be injected and
+   *                      the constructor function as last element
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#filter]]
+   */
+  def filter(name: String, filterFactory: js.Array[Any]) : Module
+
+  /**
+   * Register a provider function with the $injector.
+   *
+   * @param name The name of the instance. NOTE: the provider will be available under name + 'Provider' key.
+   * @param constructor Provider constructor function
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#provider]]
+   */
+  def provider(name: String, constructor: js.Function) : Module
+
+  /**
+   * Register a provider function with the $injector.
+   *
+   * @param name The name of the instance. NOTE: the provider will be available under name + 'Provider' key.
+   * @param constructor Array containing the names of the dependencies to be injected and
+   *                    the constructor function as last element
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#provider]]
+   */
+  def provider(name: String, constructor: js.Array[Any]) : Module
+  
+  /**
+   * Use this method to register work which should be performed when the injector is done loading all modules.
+   *
+   * @param initializationFn This function is executed when all modules have been loaded
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#run]]
+   */
+  def run(initializationFn: js.Function) : Module
+
+  /**
+   * Use this method to register work which should be performed when the injector is done loading all modules.
+   *
+   * @param initializationFn Array containing the names of the dependencies to be injected and
+   *                         the initialization function as last element
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#run]]
+   */
+  def run(initializationFn: js.Array[Any]) : Module
+
+  /**
+   * Register a service constructor which will be invoked with `new` to create the service instance.
+   *
+   * @param name The name of the service
+   * @param constructor A class constructor function
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#service]]
+   */
+  def service(name: String, constructor: js.Function) : Module
+
+  /**
+   * Register a service constructor which will be invoked with `new` to create the service instance.
+   *
+   * @param name The name of the service
+   * @param constructor A class constructor function
+   *
+   * @see [[https://docs.angularjs.org/api/ng/type/angular.Module#service]]
+   */
+  def service(name: String, constructor: js.Array[Any]) : Module
+
+
+  //------------------------------ ENHANCEMENTS ------------------------------
   /**
    * Registers the specified controller using the fully qualified class as the name of the controller.
    *
@@ -64,7 +247,6 @@ trait Module  extends js.Object {
    * @note This is a scalajs-angulate enhancement
    *
    * @tparam T
-   * @return
    */
   def serviceOf[T<:Service] : Module = macro impl.ServiceMacros.serviceOf[T]
 
@@ -73,7 +255,6 @@ trait Module  extends js.Object {
    *
    * @param name
    * @tparam T
-   * @return
    */
   def serviceOf[T<:Service](name: String) : Module = macro impl.ServiceMacros.serviceOfWithName[T]
 }
