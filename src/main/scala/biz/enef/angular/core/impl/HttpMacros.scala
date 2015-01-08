d74a54061e3049aa8d1d7b3afbb44addd5083b0c
jokade <jkspam@karchedon.de>
2015-01-08 12:27:03 +0100

    Some maintenance after renaming of project

--- src/main/scala/biz/enef/angular/core/impl/HttpMacros.scala
+++ src/main/scala/biz/enef/angular/core/impl/HttpMacros.scala
@@ -1,3 +1,8 @@
+// -   Project: scalajs-angulate (https://github.com/jokade/scalajs-angulate)
+// Description: Provides the macros for enhancements to the Angular $http API
+//
+// Copyright (c) 2015 Johannes Kastner <jkspam@karchedon.de>
+//               Distributed under the MIT License (see included file LICENSE)
 package biz.enef.angular.core.impl
 
 import scala.reflect.macros.whitebox
