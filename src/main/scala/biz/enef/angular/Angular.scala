324b8de6983f20549c7415b56b85345bfd647e47
jokade <jkspam@karchedon.de>
2015-01-10 18:03:22 +0100

    Extend Angular API
    Contributes to #14

--- src/main/scala/biz/enef/angular/Angular.scala
+++ src/main/scala/biz/enef/angular/Angular.scala
@@ -17,6 +17,78 @@ import scala.scalajs.js
 trait Angular extends js.Object {
 
   /**
+   * Use this function to manually start up an angular application.
+   *
+   * @param element
+   * @param modules
+   * @param config
+   *
+   * @see [[https://docs.angularjs.org/api/ng/function/angular.bootstrap]]
+   */
+  def bootstrap(element: js.Object, modules: js.Object = ???, config: js.Object = ???) : js.Object
+
+  /**
+   * Creates a deep copy of `source`.
+   *
+   * @param source
+   * @param destination
+   */
+  def copy(source: js.Any, destination: js.Object = ???) : js.Any = ???
+
+  /**
+   * Extends the destination object `dst` by copying own enumerable properties from the `src` object(s) to `dst`
+   *
+   * @param dst Destination object
+   * @param src Source object(s)
+   * @return reference to `dst`
+   *
+   * @see [[https://docs.angularjs.org/api/ng/function/angular.extend]]
+   */
+  def extend(dst: js.Object, src: js.Object*) : js.Object = ???
+
+  /**
+   * Deserializes a JSON string.
+   *
+   * @param json
+   *
+   * @see [[https://docs.angularjs.org/api/ng/function/angular.fromJson]]
+   */
+  def fromJson(json: String) : js.Any = ???
+
+  /**
+   * Determines if a reference is defined.
+   *
+   * @param value
+   */
+  def isDefined(value: js.Any) : Boolean = ???
+
+  /**
+   * Determines if a reference is undefined.
+   *
+   * @param value
+   */
+  def isUndefined(value: js.Any) : Boolean = ???
+
+  /**
+   * Invokes the `iterator` function once for each item in `obj`.
+   *
+   * @param obj Object to iterate over
+   * @param iterator `function(value, key, obj)`
+   * @param context Object to become context (`this`) for the iterator function
+   * @return reference to `obj`
+   *
+   * @see [[https://docs.angularjs.org/api/ng/function/angular.forEach]]
+   */
+  def forEach(obj: js.Object, iterator: js.Function3[js.Any,js.Any,js.Object,Unit], context: js.Object = ???) : js.Object = ???
+
+  /**
+   * Converts the specified string to lowercase.
+   *
+   * @param string to be converted
+   */
+  def lowercase(string: String) : String = ???
+
+  /**
    * Creates or retrieves an Angular module.
    *
    * @param name The name of the module to create or retrieve.
@@ -28,6 +100,24 @@ trait Angular extends js.Object {
    * @see [[https://docs.angularjs.org/api/ng/function/angular.module]]
    */
   def module(name: String, requires: js.Array[String] = null, configFn: js.Function = null) : Module
+
+  /**
+   * Serializes input into a JSON-formatted string.
+   *
+   * @param obj
+   * @param pretty
+   *
+   * @see [[https://docs.angularjs.org/api/ng/function/angular.toJson]]
+   */
+  def toJson(obj: js.Any, pretty: js.Any) : String = ???
+
+  /**
+   * Converts the specified string to uppercase.
+   *
+   * @param string to be converted
+   */
+  def uppercase(string: String) : String = ???
+
 }
 
 object Angular {
