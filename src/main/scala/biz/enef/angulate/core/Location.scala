273101c3284d9e16827018b9b0c9a3385f8b9753
jokade <jokade@karchedon.de>
2015-05-11 09:31:10 +0200

    Closes #44: Location.search should return Location

--- src/main/scala/biz/enef/angulate/core/Location.scala
+++ src/main/scala/biz/enef/angulate/core/Location.scala
@@ -21,7 +21,8 @@ trait Location extends js.Object with ProvidedService {
   def port(): Int = js.native
   def path(): String = js.native
   def path(path: String): Location = js.native
-  def search(search: js.Any, paramValue: js.Any = null) : js.Object = js.native
+  def search() : js.Object = js.native
+  def search(search: js.Any, paramValue: js.Any = null) : Location = js.native
   def hash(hash: String = null): String = js.native
   def replace(): Unit = js.native
 }
