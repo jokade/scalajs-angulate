1e4312336c5afda8a653d5b33ccc7bd5be0fa35d
jokade <jokade@karchedon.de>
2016-02-23 08:39:46 +0100

    Add RichQPromise

--- src/main/scala/biz/enef/angulate/core/QPromise.scala
+++ src/main/scala/biz/enef/angulate/core/QPromise.scala
@@ -9,18 +9,24 @@ import scala.scalajs.js
 
 trait QPromise extends js.Object {
 
-  def `then`(successCallback: js.Function1[js.Any,js.Any]) : QPromise = js.native
+  def `then`(successCallback: js.Function1[js.Any,_]) : QPromise = js.native
 
-  def `then`(successCallback: js.Function1[js.Any,js.Any],
-             errorCallback: js.Function1[js.Any,Unit]) : QPromise = js.native
+  def `then`(successCallback: js.Function1[js.Any,_],
+             errorCallback: js.Function1[js.Any,_]) : QPromise = js.native
 
-  def `then`(successCallback: js.Function1[js.Any,js.Any],
-             errorCallback: js.Function1[js.Any,Unit],
-             notifyCallback: js.Function1[js.Any,Unit]) : QPromise = js.native
+  def `then`(successCallback: js.Function1[js.Any,_],
+             errorCallback: js.Function1[js.Any,_],
+             notifyCallback: js.Function1[js.Any,_]) : QPromise = js.native
 
-  def `catch`(errorCallback: js.Function1[js.Any,Unit]) : QPromise = js.native
+  def `catch`(errorCallback: js.Function1[js.Any,_]) : QPromise = js.native
 
-  def `finally`(callback: js.Function1[js.Any,Unit]) : QPromise = js.native
+  def `finally`(callback: js.Function1[js.Any,_]) : QPromise = js.native
+}
+
+object QPromise {
+  implicit class RichQPromise(val p: QPromise) extends AnyVal {
+    def onSuccess(f: (js.Any)=>Unit) = p.`then`(f:js.Function1[js.Any,Unit])
+  }
 }
 
 trait QService extends ProvidedService {
